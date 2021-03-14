70c7fbabfe3d2c19a5ad9f1949751ffe7d1f8c76
Racheal Colbert <rachealcolbert16@gmail.com>
2021-03-14 16:56:27 -0700

    global state react hooks added

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,3 +1,5 @@
+import { useReducer } from "react";
+
 import {
   UPDATE_PRODUCTS,
   UPDATE_CATEGORIES,
@@ -29,3 +31,7 @@ export const reducer = (state, action) => {
       return state;
   }
 };
+
+export function useProductReducer(initialState) {
+  return useReducer(reducer, initialState);
+}
