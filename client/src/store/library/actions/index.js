7095672a136c48911934e853a6f6400b739ed1d6
Racheal Colbert <rachealcolbert16@gmail.com>
2021-03-14 21:07:05 -0700

    index.js in actions created with exports

---
+++ client/src/store/library/actions/index.js
@@ -0,0 +1,3 @@
+export { default as updateProducts } from "./updateProducts";
+export { default as updateCategories } from "./updateCategories";
+export { default as updateCurrentCategory } from "./updateCurrentCategory";
--- client/src/store/library/actions/updateCurrentCategory.js
+++ client/src/store/library/actions/updateCurrentCategory.js
@@ -2,5 +2,5 @@ import * as CONST from ".../constants";
 
 export default (currentCategory) => ({
   type: CONST.UPDATE_CURRENT_CATEGORY,
-  payload: currentCategories,
+  payload: currentCategory,
 });
