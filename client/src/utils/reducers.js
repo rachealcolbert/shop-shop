875bcce3bcefbd63c34603bbfce03860b772c62e
Racheal Colbert <rachealcolbert16@gmail.com>
2021-03-14 17:34:49 -0700

    add multiple to car test

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -4,6 +4,12 @@ import {
   UPDATE_PRODUCTS,
   UPDATE_CATEGORIES,
   UPDATE_CURRENT_CATEGORY,
+  ADD_TO_CART,
+  ADD_MULTIPLE_TO_CART,
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  CLEAR_CART,
+  TOGGLE_CART,
 } from "./actions";
 
 export const reducer = (state, action) => {
@@ -25,6 +31,13 @@ export const reducer = (state, action) => {
       return {
         ...state,
         currentCategory: action.currentCategory,
+      };
+
+    case ADD_TO_CART:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: [...state.cart, action.product],
       };
     // if it's none of these actions, do not update state at all and keep things the same!
     default:
