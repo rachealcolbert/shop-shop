c18f13c888ffdebe7f6fdc3e31dff0cbca14ead1
Racheal Colbert <rachealcolbert16@gmail.com>
2021-03-14 21:17:54 -0700

    inital selectors and exports

---
+++ client/src/store/library/reducer/index.js
@@ -0,0 +1,2 @@
+export { default as libraryInitialState } from "./initialState";
+export { default as libraryReducer } from "./reducer";
---
+++ client/src/store/library/selectors/index.js
@@ -0,0 +1,5 @@
+export const selectProducts = (state) => state.library.products;
+
+export const selectCategories = (state) => state.library.categories;
+
+export const selectCurrentCategory = (state) => state.library.currentCategory;
